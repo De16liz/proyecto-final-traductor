8735021bfe7c916fd45a28a347a6dd2cea7d789d
De16liz <deligodi1628@gmail.com>
2021-07-30 07:06:21 -0500

    proyecto final

---
+++ traductor-nativo/Config/connection.php
@@ -0,0 +1,27 @@
+<?php
+
+	class Db
+	{
+		private static $instance=NULL;
+		private static $tipo_de_base = 'mysql';
+		private static $host = 'localhost';
+		private static $nombre_de_base = 'traductor_nativo';
+		private static $usuario = 'root';
+		private static $contrasena = ''; 
+		
+		private function __construct(){}
+
+		private function __clone(){}
+		
+		public static function generarConexion(){
+			if (!isset(self::$instance)) {
+
+				$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
+				$pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND]='SET NAMES  \'UTF8\'';
+
+				self::$instance= new PDO(self::$tipo_de_base.':host='.self::$host.';dbname='.self::$nombre_de_base, self::$usuario, self::$contrasena, $pdo_options);
+			}
+			return self::$instance;
+		}
+	}
+?>
